9c16c4b332788066a589675468ae9943ec0fa10e
Scotow <contact@scotow.com>
2018-11-18 15:14:12 +0100

    add a command that display the menu in a table

--- cmd/parigo/main.go
+++ cmd/parigo/main.go
@@ -1,22 +1,99 @@
 package main
 
 import (
-	"encoding/json"
+	"flag"
 	"fmt"
+	"github.com/olekukonko/tablewriter"
+	"github.com/pkg/errors"
 	"github.com/scotow/parigo"
-	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	allFlag = flag.Bool("a", false, "display the whole current week")
+)
+
+var (
+	errTooManyMeal = errors.New("can only display menu if there is only one meal.")
 )
 
 func main() {
+	flag.Parse()
+
+	if *allFlag {
+		menu, err := parigo.Current()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, day := range menu.Days {
+			if err := displayDay(day); err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	} else {
+		today, err := currentMenuDay()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if today == nil {
+			fmt.Println("No service today.")
+			return
+		}
+
+		if err := displayDay(today); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
+
+func currentMenuDay() (*parigo.Day, error) {
 	menu, err := parigo.Current()
 	if err != nil {
-		log.Panic(err)
+		return nil, err
 	}
 
-	jsonData, err := json.MarshalIndent(menu, "", "\t")
-	if err != nil {
-		log.Panic(err)
+	now := time.Now()
+	year, month, day := now.Date()
+	today := time.Date(year, month, day, 0, 0, 0, 0, now.Location())
+
+	for _, day := range menu.Days {
+		if day.Time.Equal(today) {
+			return day, nil
+		}
 	}
 
-	fmt.Println(string(jsonData))
+	return nil, nil
+}
+
+func displayDay(day *parigo.Day) error {
+	if len(day.Meals) != 1 {
+		return errTooManyMeal
+	}
+
+	day.Title = strings.Replace(day.Title, "Menu du ", "", -1)
+	meal := day.Meals[0]
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetRowLine(true)
+	table.SetAutoWrapText(false)
+	table.SetAlignment(tablewriter.ALIGN_CENTER)
+
+	table.SetHeader([]string{day.Title})
+	table.SetHeaderColor(tablewriter.Colors{tablewriter.Bold})
+
+	for _, part := range meal.Parts {
+		table.Append([]string{strings.Join(part.Plates, "\n")})
+	}
+
+	table.Render()
+
+	return nil
 }
